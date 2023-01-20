7f8cbb140bcb1ed9a357ba88c8a06e141567ccd0
WesleyNichols <wesley.nichols@me.com>
2023-01-19 17:02:21 -0800

    :)

--- src/main/java/survival/utilities/survivalutilities/commands/MapCommand.java
+++ src/main/java/survival/utilities/survivalutilities/commands/MapCommand.java
@@ -1,24 +1,29 @@
 package survival.utilities.survivalutilities.commands;
 
-import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.event.ClickEvent;
-import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.event.HoverEvent;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+import survival.utilities.survivalutilities.SurvivalUtilities;
+
+import java.util.Objects;
 
 public class MapCommand implements CommandExecutor {
 
     public static String getCommand = "map";
 
-    private static final String mapLink = "https://map.beeboxmc.com";
-
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (sender instanceof Player player) {
-            player.sendMessage(Component.text("Click here to open the map!").color(NamedTextColor.GOLD).clickEvent(ClickEvent.openUrl(mapLink)));
+            player.sendMessage(LegacyComponentSerializer.legacyAmpersand()
+                    .deserialize(Objects.requireNonNull(SurvivalUtilities.getInstance().getConfig().getString("map")))
+                    .clickEvent(ClickEvent.openUrl(Objects.requireNonNull(SurvivalUtilities.getInstance().getConfig().getString("map-link"))))
+                    .hoverEvent(HoverEvent.showText(LegacyComponentSerializer.legacyAmpersand()
+                            .deserialize(Objects.requireNonNull(SurvivalUtilities.getInstance().getConfig().getString("map-hover"))))));
             return true;
         }
         return false;
